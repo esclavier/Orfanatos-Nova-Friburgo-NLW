7acfbf953037cf61d9ba1163cc804280eaaa47a8
esclavier <philippe.esclavier@outlook.com>
2020-10-16 17:46:25 -0300

    Update Front-End

--- Orfanatos-Nova Friburgo-NLW/public/scripts/page-create-orphanege.js
+++ Orfanatos-Nova Friburgo-NLW/public/scripts/page-create-orphanege.js
@@ -35,14 +35,18 @@ map.on("click", (event) => {
 
 function addPhotoField() {
   // pegar o container de images
+
   const container = document.querySelector("#images");
   // pegar o container e duplicar
+
   const fieldsContainer = document.querySelectorAll(".new-upload");
   // fazer o clone da ultima imagem adicionada
+
   const newFieldContainer = fieldsContainer[
     fieldsContainer.length - 1
   ].cloneNode(true);
   //verificar se campo vazio
+
   const input = newFieldContainer.children[0];
 
   if (input.value == "") {
@@ -56,12 +60,12 @@ function addPhotoField() {
   container.appendChild(newFieldContainer);
 }
 
-function deleteField() {
-  const span = Event.currentTarget;
+function deleteField(event) {
+  const span = event.currentTarget;
 
   const fieldsContainer = document.querySelectorAll(".new-upload");
 
-  if (fieldsContainer <= 1) {
+  if (fieldsContainer.length < 2) {
     //limpar campo
     span.parentNode.children[0].value = "";
     return;
@@ -72,7 +76,7 @@ function deleteField() {
 
 //selecionar sim ou não
 
-function toggleSelect(Event) {
+function toggleSelect(event) {
   //retirar a class active
   document.querySelectorAll(".button-select button").forEach(function (button) {
     button.classList.remove("active");
@@ -80,8 +84,8 @@ function toggleSelect(Event) {
 
   //colocar class active
 
-  const button = Event.currentTarget;
-  button.classListadd("active");
+  const button = event.currentTarget;
+  button.classList.add("active");
 
   //atualizar input
   const input = document.querySelector('[name="open_on_weekends"]');
